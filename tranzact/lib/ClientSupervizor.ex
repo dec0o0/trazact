b6a15ecded9f8f79e45686dc8ca0b772d9824c6b
Dumitrache <alexandru.dumitraiche@gmail.com>
2017-04-24 16:49:03 +0300

    Revert "global"
    This reverts commit a0412c0362d8ca07e53b2df667ab9247ba2e0cef, reversing
    changes made to 028a41939d65968177b83260cd156f53a395387a.

--- tranzact/lib/ClientSupervizor.ex
+++ tranzact/lib/ClientSupervizor.ex
@@ -1,7 +1,7 @@
-defmodule Client.Supervizor do
+defmodule ClientSupervizor do
 	use Supervisor
 
-	@name Client.Supervizor
+	@name ClientSupervizor
 
 	def start_link do
 		Supervisor.start_link(__MODULE__, :ok, name: @name)
@@ -14,7 +14,6 @@ defmodule Client.Supervizor do
 	def init(:ok) do
 		children = [
 			worker(Client, [], restart: :temporary)
-			
 		]
 
 		supervise(children, strategy: :simple_one_for_one)
